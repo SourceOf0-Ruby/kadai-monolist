36b0c6016d56b69bcb1ba574bdbea8e1105adbb8
Source of 0 <SourceOf0@gmail.com>
2018-04-06 03:05:27 +0000

    create user registration system

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,17 @@
+module UsersHelper
+  
+  # Gravatar(https://ja.gravatar.com/)に登録されている画像のurlを取得する
+  # @param user: 対象のユーザのモデルインスタンス
+  # @param options: Gravatarの仕様に沿って追加するオプションの内容
+  #                 size: URL?s=[size] サイズ指定、1～2048pxまで
+  def gravatar_url(user, options = { size: 80 })
+    # 小文字のみメアドをMD5でハッシュに変換
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase);
+    size = options[:size];
+    
+    # MEMO: URL末尾はデフォルトイメージの指定
+    # 参考: https://ja.gravatar.com/site/implement/images#default-image
+    return "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=mm";
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   
+  # トップページ
   root to: 'toppages#index';
+  
+  # ユーザ周り
+  get 'signup', to: 'users#new';
+  resources :users, only: [:show, :new, :create];
   
 end
